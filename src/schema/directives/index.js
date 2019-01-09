dac4ca00ddef91117da16f455eb9dfdfe5a0c3b5
Nater Jorde <nater@iamnater.com>
2019-01-09 13:08:16 -0600

    Step 8 Add instructions for Input Validation via Directives

--- src/schema/directives/index.js
+++ src/schema/directives/index.js
@@ -1,9 +1,12 @@
 import { typeDef as dateTypeDef, dateDirectives } from './date';
+import { typeDef as emailTypeDef, emailDirectives } from './email';
 
 export const directiveTypeDefs = [
   dateTypeDef,
+  emailTypeDef,
 ];
 
 export const schemaDirectives = {
   ...dateDirectives,
+  ...emailDirectives,
 };
